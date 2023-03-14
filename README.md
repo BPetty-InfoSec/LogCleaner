c660fb41d3b2b749bc400d003e50afa951154517
BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>
2023-03-13 21:09:33 -0400

    add file output
    Added edited file output. Swapped format style to .json.
    
    Signed-off-by: BPetty_InfoSec <62268810+BPetty-InfoSec@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,8 +1,34 @@
 # logcleaner.py
 
----
-
 This is a tiny app to clean up log files and format them using RegEx.
 
-Different formatting can be specified using individual files in the formats directory.
-These files should be composed of lines consisting of RegEx statements which will be executed consecutively.
+Technically, this could be used with any type of text file for formatting.
+
+The format files (you can have as many as you like) should live in the `formats`
+directory. These files are json files, consisting of an array of objects. Each object
+has two required attributes, and one semi-optional attribute. Each item in the file
+will be run as a substitution against the selected log file sequentially.
+
+## Format File Attributes
+
+- `action`: This attribute determines what type of action this item will perform.
+  - `delete`: This will substitute "" (nothing) for the `pattern` provided, effectively deleting all occurrences of the pattern
+  - `replace`: This will substitute the `replace` attribute for `pattern` throughout the file
+- `pattern`: This is the regEx pattern that will be matched against in the log file
+- `replace`: This attribute is not required (and is ignored) for `delete` actions, but is necessary for for `replace` actions. The value of this attribute will be substituted for the `pattern` throughout the log file
+
+## Sample Format File Structure
+
+```json
+[
+    {
+        "action": "delete",
+        "pattern": " id=\"\\d+\""
+    },
+    {
+        "action": "replace",
+        "pattern": "<metadata>",
+        "replace": "<!-- <metadata> -->"
+    }
+]
+```
---
+++ logcleaner.py
@@ -0,0 +1,85 @@
+# An app that will format text files using RegEx from a format file.
+# Each format will be a .json file consisting of an array of objects.
+# Each object requires an "action" and a "pattern" attribute
+# Actions (currently) consist of replace and delete
+# The Replace action requires a "replace" attribute as well, which will
+#   be used to replace the pattern on search.
+# The files will exist in the "formats" subdirectory.
+# Command-line arguments will be:
+#   - the name of the format to be used
+#   - the name of the file to be formatted
+#   - (optionally) a "formats" argument that will display a list of available formats and a short description
+
+import os, re, json
+import argparse
+
+parser = argparse.ArgumentParser(description='Read command line arguments')
+parser.add_argument('logFormat', help="""The name of the format to be applied to the log file.
+                    These format files are located in the "formats" subdirectory""")
+parser.add_argument('logFile', help="The log file to read")
+parser.add_argument('-o', '--outputFile',
+                    help="Name of file to store results of formatting.")
+args = parser.parse_args()
+
+if os.name == 'nt':
+    osSep = "\\"
+else:
+    osSep = "/"
+
+def main(appPath):
+    if args.outputFile:
+        outputFile = args.outputFile
+    else:
+        outputFile = CreateGenericLogfile()
+    with open(outputFile, 'w') as outputLog:
+        with open(args.logFile, 'rt') as readLog:
+            editedLog = readLog.read()
+            formats = GetFormats(appPath + osSep + "formats" + osSep + args.logFormat + '.json')
+            for item in formats:
+                if item["action"]:
+                    match item["action"].lower():
+                        case "delete":
+                            if item["pattern"]:
+                                editedLog = re.sub(item["pattern"],"",editedLog)
+                            else:
+                                print("You must have a pattern for each format")
+                                continue
+                        case "replace":
+                            if item["replace"]:
+                                editedLog = re.sub(item["pattern"],item["replace"],editedLog)
+                            else:
+                                print("You must have a replace item for any replace pattern")
+                                continue
+                        case _:
+                            continue
+                else:
+                    print("Every format line must have an action")
+            formatString = "Formatting log file [" + args.logFile + \
+                "] with format-file [" + args.logFormat + \
+                "] and saving to [" + outputFile + "]"
+            print(formatString)
+            outputLog.write(editedLog)
+
+
+def CreateGenericLogfile(iterDex=0):
+    if iterDex == 0:
+        iterToken = ""
+    else:
+        iterToken = str(iterDex)
+    outputLogName = "parsedLog" + iterToken + ".log"
+    if os.path.isfile(outputLogName):
+        outputLogName = CreateGenericLogfile(iterDex+1)
+    else:
+        with open(outputLogName, 'x') as createLog:
+            pass
+    return outputLogName
+
+
+def GetFormats(formatPath):
+    with open(formatPath, 'r') as formatFile:
+        return json.load(formatFile)
+
+
+if __name__ == '__main__':
+    appPath = os.path.dirname(__file__)
+    main(appPath)
